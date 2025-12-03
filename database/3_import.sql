9748501b08e79a197888dd78eff333909dc4d15f
opilane <meelis.n@gmail.com>
2025-12-03 11:05:08 +0200

    MEELIS-BEB-DATABASE

--- database/3_import.sql
+++ database/3_import.sql
@@ -1,5 +1,5 @@
 INSERT INTO bmx.role (id, name) VALUES (default, 'admin');
 INSERT INTO bmx.role (id, name) VALUES (default, 'customer');
 
-INSERT INTO bmx."user" (id, role_id, name, password, status) VALUES (default, 1, 'admin', '123', 'A');
-INSERT INTO bmx."user" (id, role_id, name, password, status) VALUES (default, 2, 'user', '123', 'A');
+INSERT INTO bmx."user" (id, role_id, name, password, status, email) VALUES (default, 1, 'admin', '123', 'A', null);
+INSERT INTO bmx."user" (id, role_id, name, password, status, email) VALUES (default, 2, 'user', '123', 'A', null);
