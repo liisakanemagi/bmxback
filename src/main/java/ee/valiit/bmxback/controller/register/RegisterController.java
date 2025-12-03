ca93defeb8ba5cc4ee3380b3a1316b3cb2d36d86
opilane <meelis.n@gmail.com>
2025-12-03 14:22:59 +0200

    MEELIS-BEB-NEW-USER-COMPLETE

--- src/main/java/ee/valiit/bmxback/controller/register/RegisterController.java
+++ src/main/java/ee/valiit/bmxback/controller/register/RegisterController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
