29144fb063fb633bf429c71c93ff9c18199442a4
liisakanemagi <liisa.kanemagi@gmail.com>
2025-12-08 10:11:57 +0200

    LIISA-BEB-LOCATION-COMPLETE

--- src/main/java/ee/valiit/bmxback/controller/county/CountyController.java
+++ src/main/java/ee/valiit/bmxback/controller/county/CountyController.java
@@ -1,23 +1,18 @@
 package ee.valiit.bmxback.controller.county;
-
 import ee.valiit.bmxback.controller.county.dto.CountyInfo;
 import ee.valiit.bmxback.service.CountyService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-
 public class CountyController {
 
     private final CountyService countyService;
-
     @GetMapping("/counties")
     @Operation(
             summary = "Leiab süsteemist kõik maakonnad.",
--- src/main/java/ee/valiit/bmxback/controller/location/LocationController.java
+++ src/main/java/ee/valiit/bmxback/controller/location/LocationController.java
@@ -1,8 +1,13 @@
 package ee.valiit.bmxback.controller.location;
 
 import ee.valiit.bmxback.controller.location.dto.LocationDto;
-import ee.valiit.bmxback.persistence.user.UserRepository;
+import ee.valiit.bmxback.infrastructure.error.ApiError;
 import ee.valiit.bmxback.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,10 @@ public class LocationController {
     private final LocationService locationService;
 
     @PostMapping("/location")
+    @Operation(summary = "Uue asukoha lisamine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "403", description ="Sõidukoht on juba olemas (errorCode 113)", content = @Content(schema = @Schema(implementation = ApiError.class)))})
     public void addLocation(@RequestBody @Valid LocationDto locationDto, @RequestParam Integer userId) {
         locationService.addLocation(locationDto, userId);
     }
