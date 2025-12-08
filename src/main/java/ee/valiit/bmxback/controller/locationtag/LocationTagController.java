384611546db24563d561f25cc2f937f6f239b9cd
opilane <meelis.n@gmail.com>
2025-12-08 16:14:02 +0200

    MEELIS-BEB-LOCATION IMAGE WIP 7

--- src/main/java/ee/valiit/bmxback/controller/locationtag/LocationTagController.java
+++ src/main/java/ee/valiit/bmxback/controller/locationtag/LocationTagController.java
@@ -1,17 +1,23 @@
 package ee.valiit.bmxback.controller.locationtag;
 
 import ee.valiit.bmxback.controller.locationtag.dto.LocationTagDto;
+import ee.valiit.bmxback.service.locationtag.LocationTagService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class LocationTagController {
-    @PostMapping("/location/tag")
-    public void addLocationTag(@RequestBody @Valid LocationTagDto locationTagDto) {
 
+    private final LocationTagService locationTagService;
+
+    @PostMapping("/location-tags")
+    public void addLocationTag(@RequestBody @Valid LocationTagDto locationTagDto) {
+        locationTagService.addLocationTag(locationTagDto);
     }
 }
---
+++ src/main/java/ee/valiit/bmxback/controller/tag/TagController.java
@@ -0,0 +1,10 @@
+package ee.valiit.bmxback.controller.tag;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TagController {
+
+}
