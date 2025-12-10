e0a72034d1554361a8672926f108e313ffd5530f
opilane <meelis.n@gmail.com>
2025-12-10 09:32:04 +0200

    MEELIS-BEB-LOCATION IMAGE WIP 9

--- src/main/java/ee/valiit/bmxback/service/LocationTagService.java
+++ src/main/java/ee/valiit/bmxback/service/LocationTagService.java
@@ -2,6 +2,8 @@ package ee.valiit.bmxback.service;
 
 import ee.valiit.bmxback.controller.locationtag.dto.LocationTagDto;
 import ee.valiit.bmxback.persistence.location.Location;
+import ee.valiit.bmxback.persistence.locationtag.LocationTag;
+import ee.valiit.bmxback.persistence.locationtag.LocationTagRepository;
 import ee.valiit.bmxback.persistence.tag.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class LocationTagService {
     private final LocationService locationService;
     private final TagService tagService;
+    private final LocationTagRepository locationTagRepository;
 
     public void addLocationTag(@Valid LocationTagDto locationTagDto) {
         Location location = locationService.getValidLocation(locationTagDto.getLocationId());
@@ -19,5 +22,11 @@ public class LocationTagService {
 
 //        Lisada eraldi "lisa uus tag" nupp fronti ja see kasutab TagControllerit.
 //        LocationTagController kasutab ainult juba loodud tag-e. Kontrollib asukoha ja tag-i ID-sid ja paneb need kokku LocationTag tabelisse.
+        LocationTag locationTag = new LocationTag();
+        locationTag.setLocation(location);
+        locationTag.setTag(tag);
+        locationTagRepository.save(locationTag);
+
     }
+
 }
