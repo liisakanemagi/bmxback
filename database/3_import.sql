a04f2d6cce8ca986a9d96ebdef001ef759cf3b25
Meelis-n <meelis.n@gmail.com>
2025-12-12 10:21:09 +0200

    BEB

--- database/3_import.sql
+++ database/3_import.sql
@@ -30,6 +30,16 @@ INSERT INTO bmx.tag (id, name, status) VALUES (default, 'kesktase', 'A');
 INSERT INTO bmx.tag (id, name, status) VALUES (default, 'ekspert', 'A');
 INSERT INTO bmx.tag (id, name, status) VALUES (default, 'pro', 'A');
 
+
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 6, 'Haapsalu Skatepark', 'Lihula mnt 10, Haapsalu', 23.541411, 58.936361, 'Suur park ja lõbus park, aga praeguseks juba amortiseerunud. Vineeris on auke.', 'P', 0.0, '2025-12-11 18:31:36.105150');
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 1, 'Kanutiaia rulapark', 'Aia tn 12, 10111 Tallinn', 24.752162, 59.439133, 'Tüüpiline Rhino Parks-i konstruktsioon. Libe ja mitte eriti fun.', 'P', 0.0, '2025-12-11 18:36:41.517111');
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 12, 'Tartu Tähtvere Skatepark', 'F. Tuglase tn, 51006 Tartu', 26.698958, 58.387545, 'Suur striidile orienteeritud skatepark. Ruumi on kõvasti. On ka parajalt sügav bowli osa, kus kihutada saab.', 'P', 0.0, '2025-12-11 18:46:54.196243');
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 2, 12, 'Tartu Tähtvere Pumptrack', 'F. Tuglase tn, 51006 Tartu', 26.699637, 58.387232, 'Kahe hüppega asjalik pumptrack.', 'P', 0.0, '2025-12-11 19:00:00.866132');
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 3, 1, 'Skoone Spot', 'Suurtüki tn 12, 10133 Tallinn', 24.746599, 59.442888, 'Reilid ja kickerid.', 'P', 0.0, '2025-12-11 19:35:05.696771');
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 3, 5, 'fsjytjhsd', 'fsgsgfjsb', 25.575829, 58.887962, 'dbgbgdf', 'P', 0.0, '2025-12-11 19:38:31.871888');
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 2, 'Kärdla Skatepark', 'Põllu tn 27, Kärdla, 92414 Hiiu maakond', 22.757610, 58.997309, 'Madalad obstaaklid, pigem mõeldud päris algajatele või siis tõukeratasele ja ruladele.', 'P', 0.0, '2025-12-11 21:33:07.224710');
+INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 2, 2, 'Viscosa Kultuuritehase Pumptrack', 'Jäätme, Kõrgessaare, 92201 Hiiu maakond', 22.469235, 58.980669, 'Lihtsam pumptrack otse kultuuri südames.', 'P', 0.0, '2025-12-11 22:05:42.898798');
+
 INSERT INTO bmx.location_tag (id, location_id, tag_id) VALUES (default, 6, 3);
 INSERT INTO bmx.location_tag (id, location_id, tag_id) VALUES (default, 1, 4);
 INSERT INTO bmx.location_tag (id, location_id, tag_id) VALUES (default, 2, 1);
@@ -51,14 +61,8 @@ INSERT INTO bmx.location_rating (id, user_id, location_id, rating, created_at) V
 INSERT INTO bmx.location_rating (id, user_id, location_id, rating, created_at) VALUES (default, 2, 3, 4, '2025-12-11 22:30:46.000000');
 INSERT INTO bmx.location_rating (id, user_id, location_id, rating, created_at) VALUES (default, 2, 5, 5, '2025-12-11 22:30:47.000000');
 
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 6, 'Haapsalu Skatepark', 'Lihula mnt 10, Haapsalu', 23.541411, 58.936361, 'Suur park ja lõbus park, aga praeguseks juba amortiseerunud. Vineeris on auke.', 'P', 0.0, '2025-12-11 18:31:36.105150');
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 1, 'Kanutiaia rulapark', 'Aia tn 12, 10111 Tallinn', 24.752162, 59.439133, 'Tüüpiline Rhino Parks-i konstruktsioon. Libe ja mitte eriti fun.', 'P', 0.0, '2025-12-11 18:36:41.517111');
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 12, 'Tartu Tähtvere Skatepark', 'F. Tuglase tn, 51006 Tartu', 26.698958, 58.387545, 'Suur striidile orienteeritud skatepark. Ruumi on kõvasti. On ka parajalt sügav bowli osa, kus kihutada saab.', 'P', 0.0, '2025-12-11 18:46:54.196243');
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 2, 12, 'Tartu Tähtvere Pumptrack', 'F. Tuglase tn, 51006 Tartu', 26.699637, 58.387232, 'Kahe hüppega asjalik pumptrack.', 'P', 0.0, '2025-12-11 19:00:00.866132');
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 3, 1, 'Skoone Spot', 'Suurtüki tn 12, 10133 Tallinn', 24.746599, 59.442888, 'Reilid ja kickerid.', 'P', 0.0, '2025-12-11 19:35:05.696771');
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 3, 5, 'fsjytjhsd', 'fsgsgfjsb', 25.575829, 58.887962, 'dbgbgdf', 'P', 0.0, '2025-12-11 19:38:31.871888');
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 1, 2, 'Kärdla Skatepark', 'Põllu tn 27, Kärdla, 92414 Hiiu maakond', 22.757610, 58.997309, 'Madalad obstaaklid, pigem mõeldud päris algajatele või siis tõukeratasele ja ruladele.', 'P', 0.0, '2025-12-11 21:33:07.224710');
-INSERT INTO bmx.location (id, user_id, location_type_id, county_id, name, address, lng, lat, description, status, average_rating, created_at) VALUES (default, 2, 2, 2, 'Viscosa Kultuuritehase Pumptrack', 'Jäätme, Kõrgessaare, 92201 Hiiu maakond', 22.469235, 58.980669, 'Lihtsam pumptrack otse kultuuri südames.', 'P', 0.0, '2025-12-11 22:05:42.898798');
+
+
 
 INSERT INTO bmx.location_rating (id, user_id, location_id, rating, created_at) VALUES (default, 1, 1, 4, '2025-12-09 22:26:27.000000');
 INSERT INTO bmx.location_rating (id, user_id, location_id, rating, created_at) VALUES (default, 1, 4, 5, '2025-12-08 23:27:53.000000');
